5ec4d6d4ea833386614a909d1a7e76777e39cfc5
Seongjin Jang <seojang@c2r16s3.42gyeongsan.kr>
2024-09-30 02:26:50 +0900

    redir_test_up

--- test/dup_test.c
+++ test/dup_test.c
@@ -6,25 +6,36 @@
 /*   By: seojang <seojang@student.42gyeongsan.kr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 22:13:37 by seojang           #+#    #+#             */
-/*   Updated: 2024/09/29 02:13:59 by seojang          ###   ########.fr       */
+/*   Updated: 2024/09/29 20:49:52 by seojang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <fcntl.h>
 #define BUFFER_SIZE 42
 
-int	main()
+void	change_standard_input(void)
 {
-	char	*buf = "HELLO WORLD!\n";
-	int		*file_fd = open("./test", O_WRONLY);
+	int	fd = open("test_txt.txt", O_RDONLY);
+	dup2(fd, STDIN_FILENO);
+	close(fd);
+}
 
-	int		new_stdout = dup(STDOUT_FILENO);
-	dup2(file_fd, STDOUT_FILENO);
+int	main(int ac, char **av, char **envp)
+{
+	char	*path;
+	char	**argument;
 
-	write(new_stdout, buf, BUFFER_SIZE);
+	argument = (char **)malloc(sizeof(char *) * 1);
+	argument[0] = strdup("cat");
+	path = strdup("/usr/bin/cat");
+	//change_standard_input();
+	execve(path, argument, envp);
+	perror("ERROR");
+	exit(EXIT_FAILURE);
 }
